83dce5ee52a479167a252e606080e4930d8ac434
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-08-19 18:28:07 +0200

    Added the icons to the login page. As for the CSS file, the height of the login container has been adjusted to accommodate the icons with enough white space.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -68,6 +68,17 @@ const LoginPage = (props: User) => {
           <button type="submit" className="login-button">
             Login
           </button>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+          </div>
         </form>
       </div>
       {/* <Footer /> */}

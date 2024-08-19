28ebf7fff219e348661a83fe71bd3193e3b58d6c
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-08-19 18:09:40 +0200

    Added my stickerbook tag in _document.tsx! This gives me the ability to refer to my icons, in this case my social icons, in my register-page file. In the CSS file, is just the customization of said icons.

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -86,6 +86,17 @@ const RegistryPage = () => {
           <button type="submit" className="register-button">
             Register
           </button>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+          </div>
         </form>
       </div>
       <Footer />

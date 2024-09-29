8d1631c6d3fa5d46b2230e84418dfc09e339db8a
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-29 16:50:54 +0200

    Started to create a sidebar for the admin backroom

--- src/pages/admindashboard.tsx
+++ src/pages/admindashboard.tsx
@@ -1,7 +1,18 @@
 const AdminDashboard = () => {
   return (
-    <div>
+    <div className="dashboard-container">
       <h1>Hello dashboard</h1>
+      <ul>
+        <li>
+          <a href="admin/mytables"></a>Manage Tables
+        </li>
+        <li>
+          <a href="admin/myorders">Manage Orders</a>
+        </li>
+        <li>
+          <a href="admin/menu">Manage Menu</a>
+        </li>
+      </ul>
     </div>
   );
 };

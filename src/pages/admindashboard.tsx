8241fa3fcae3f89467fa707debf065c356f51def
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-30 14:57:05 +0200

    Minor update to the skeletal structure of the page

--- src/pages/admindashboard.tsx
+++ src/pages/admindashboard.tsx
@@ -5,18 +5,28 @@ const AdminDashboard = () => {
   return (
     <div className="dashboard-container">
       <Navbar />
-      <h1>Welcome to the Backroom</h1>
-      <ul className="dashboard-sidebar">
-        <li className="dash-item">
-          <a href="admin/mytables">Manage Tables</a>
-        </li>
-        <li className="dash-item">
-          <a href="admin/myorders">Manage Orders</a>
-        </li>
-        <li className="dash-item">
-          <a href="admin/menu">Manage Menu</a>
-        </li>
-      </ul>
+      <div className="dashboard-main">
+        <div className="dashboard-sidebar">
+          <ul>
+            <li className="dash-item">
+              <a href="admin/mytables">Manage Tables</a>
+            </li>
+            <li className="dash-item">
+              <a href="admin/myorders">Manage Orders</a>
+            </li>
+            <li className="dash-item">
+              <a href="admin/menu">Manage Menu</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h1>Welcome to the Backroom</h1>
+        <p>
+          Please select an option from the sidebar to manage your restaurant
+          settings
+        </p>
+      </div>
     </div>
   );
 };

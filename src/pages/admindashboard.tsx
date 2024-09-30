3a783d09d2c49977562c4c45b818355d4a66d7bc
Brandon Lambertus <lambertus.brandon@gmail.com>
2024-09-30 14:46:30 +0200

    Started to work on the CSS for the dashboard

--- src/pages/admindashboard.tsx
+++ src/pages/admindashboard.tsx
@@ -1,18 +1,19 @@
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 const AdminDashboard = () => {
   return (
     <div className="dashboard-container">
       <Navbar />
       <h1>Welcome to the Backroom</h1>
-      <ul>
-        <li>
+      <ul className="dashboard-sidebar">
+        <li className="dash-item">
           <a href="admin/mytables">Manage Tables</a>
         </li>
-        <li>
+        <li className="dash-item">
           <a href="admin/myorders">Manage Orders</a>
         </li>
-        <li>
+        <li className="dash-item">
           <a href="admin/menu">Manage Menu</a>
         </li>
       </ul>
